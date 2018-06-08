b88b886dfc1b0cb72c0e17a67de2bc391db22b58
KyoungWan <coruddhks@gmail.com>
2018-06-08 19:06:32 +0900

    First test code

--- src/fpga_api.cpp
+++ src/fpga_api.cpp
@@ -5,6 +5,10 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
+#include <math.h>
+#include <cstdio>
+#include <cstdlib>
+
 #define DATA_SIZE SIZE*(SIZE+1)*sizeof(float) // fpga bram data size
 
 #define min(x,y) (((x)<(y))?(x):(y))
@@ -46,8 +50,54 @@ void FPGA::largeMV(const float* large_mat, const float* input,
 {
     // write down your code here.
 
-    float* vec = this->vector();
-    float* mat = this->matrix();
+    float* vec = this->vector(); // 64
+    float* mat = this->matrix(); // 64 * 64
 
+    //making multiple of 64
+    int col_cnt=(int)ceil(M/64);
+    int row_cnt=(int)ceil(N/64);
+    int col = col_cnt *64;
+    int row = row_cnt *64;
+
+    float *new_mat= (float*)malloc(col*row*sizeof(float));
+    float *new_vec= (float*)malloc(row*sizeof(float));
+    //copy large_mat, input into matrix
+    //make all 0 , redundant by malloc??????????
+    for(int i=0; i<row_cnt*SIZE; i++) {
+      for(int j=0; j<col_cnt*SIZE; j++){
+        *(new_mat+row_cnt*SIZE*i+j) = 0;
+      }
+    }
+    for(int i=0; i<M; i++) {
+      for(int j=0; j<N; j++){
+        *(new_mat+row_cnt*SIZE*i+j) = *(large_mat+M*i +j );
+      }
+    }
+    //printf
+    printf("print Matrix start \n");
+    for(int i=0; i<col_cnt*SIZE; i++) {
+      for(int j=0; j<row_cnt*SIZE; j++){
+        //printf("[%d][%d]=%.f ", i, j, *(new_mat+row_cnt*i +j));
+        printf("%.1f ", *(new_mat+row_cnt*i +j));
+      }
+      printf("\n");
+    }
+    printf("print Matrix end \n");
     // write down your code here.
+    float *temp = (float*)malloc(M*sizeof(float));
+    memset(temp, 0, M*sizeof(float));
+
+    const float* ptemp;
+    for(int i=0; i<col; i=i+SIZE) {
+      for(int j=0; j<row; j=j+SIZE){
+        memcpy(mat, new_mat+i*col+j, (SIZE*SIZE)*sizeof(float));
+        memcpy(vec, new_vec+i, (SIZE)*sizeof(float));
+        ptemp = this->run();
+
+        for(int i=0+SIZE*j; i<SIZE+SIZE*j; i++){
+          *(temp+i) = *(temp+i)+*(ptemp+i);
+        }
+      }
+    }
+    output = temp;
 }
